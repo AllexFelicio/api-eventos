8d9af9d3abae0440fe88c9739377713caa667996
Alex <junioralex106@gmail.com>
2023-11-07 19:51:15 -0300

    url local corrigida

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -20,7 +20,7 @@ const eventRoutes = require('./routes/eventRoutes');
 const placeRoutes = require('./routes/placeRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, options));
 app.use('/events', eventRoutes);
 app.use('/places', placeRoutes);
 app.use('/tickets', ticketRoutes);
