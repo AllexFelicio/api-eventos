1c514857795f465832395d1ae3c5d7632a84ed01
Alex <junioralex106@gmail.com>
2023-11-07 21:46:20 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const swaggerJsDoc = require('swagger-jsdoc')
 const cors = require('cors');
 require('dotenv').config();
 
-
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const app = express();
 app.use(express.json());
