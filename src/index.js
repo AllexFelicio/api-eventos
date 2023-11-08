5129ccbf75717848bd5602468ab25de8c9f2f53b
Alex <junioralex106@gmail.com>
2023-11-07 21:41:53 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -24,41 +24,45 @@ const placeRoutes = require('./routes/placeRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
 
 const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Library API",
-        version: "1.0.0",
-        description: "A simple Express Library API",
-        termsOfService: "http://example.com/terms/",
-        contact: {
-          name: "API Support",
-          url: "http://www.exmaple.com/support",
-          email: "support@example.com",
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
       },
-      servers: [
-        {
-          url: "https://nodejs-swagger-api.vercel.app/",
-          description: "My API Documentation",
-        },
-      ],
     },
-    // This is to call all the file
-    apis: ["src/**/*.js"],
-  };
-  
-  const specs = swaggerJsDoc(options);
-  // app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-  
-  app.use(
-    "/api-docs",
-    swaggerUI.serve,
-    swaggerUI.setup(specs, { customCssUrl: CSS_URL })
-  );
-  
+    servers: [
+      {
+        url: "http://localhost:3001",
+        description: "API local"
+      },
+      {
+        url: "https://api-eventos-allexfelicio.vercel.app",
+        description: "API de produção"
+    }
+    ],
+  },
+  // This is to call all the file
+  apis: ["src/**/*.js"],
+};
 
-//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
+
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/events', eventRoutes);
 app.use('/places', placeRoutes);
 app.use('/tickets', ticketRoutes);
