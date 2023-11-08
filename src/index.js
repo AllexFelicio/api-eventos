56643f8882d13c30977e1d7b8345dba40133d136
Alex <junioralex106@gmail.com>
2023-11-07 21:43:06 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-const swaggerJsDoc = require('swagger-jsdoc');
 const cors = require('cors');
 require('dotenv').config();
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 
 const app = express();
@@ -22,44 +19,6 @@ const port = 3001;
 const eventRoutes = require('./routes/eventRoutes');
 const placeRoutes = require('./routes/placeRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-      termsOfService: "http://example.com/terms/",
-      contact: {
-        name: "API Support",
-        url: "http://www.exmaple.com/support",
-        email: "support@example.com",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:3001",
-        description: "API local"
-      },
-      {
-        url: "https://api-eventos-allexfelicio.vercel.app",
-        description: "API de produção"
-    }
-    ],
-  },
-  // This is to call all the file
-  apis: ["src/**/*.js"],
-};
-
-const specs = swaggerJsDoc(options);
-// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
-
-app.use(
-  "/api-docs",
-  swaggerUI.serve,
-  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
-);
 
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
